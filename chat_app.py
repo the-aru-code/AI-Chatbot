8374f745d4266cd2338bfa56accb529da9131932
the-aru-code <saggopandey@gmail.com>
2025-12-29 15:34:19 +0530

    Add developer footer to chat application
    Added a footer with developer information.

--- chat_app.py
+++ chat_app.py
@@ -18,7 +18,7 @@ model = genai.GenerativeModel("gemini-2.5-flash")
 
 # Page config
 st.set_page_config(page_title="Gemini Chat", page_icon="🤖")
-st.title("🤖 Chatbot")
+st.title("🤖Chatbot")
 
 # Initialize chat history
 if "chat" not in st.session_state:
@@ -57,7 +57,7 @@ if user_prompt:
             for char in assistant_reply:
                 typed_text += char
                 placeholder.markdown(typed_text)
-                time.sleep(0.0002)  # typing speed (adjust)
+                time.sleep(0.02)  # typing speed (adjust)
 
     st.session_state.messages.append(
         {"role": "assistant", "content": assistant_reply}
@@ -69,6 +69,7 @@ if st.sidebar.button("🔄 Reset Conversation"):
     st.session_state.messages = []
     st.experimental_rerun()
 
+st.markdown('**design and developed by:Arushi Pandey**')
 
 
 
